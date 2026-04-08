88869f5cf9f45eb054b132594cb30eb5aa92d386
purabayre <purabayre293@gmail.com>
2026-04-08 14:46:36 +0530

    final

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { generateMonthlyPDF } = require("../controllers/pdfController");
 
-// GET /api/pdf/monthly?month=&year=
+
 router.get("/monthly", generateMonthlyPDF);
 
 module.exports = router;
